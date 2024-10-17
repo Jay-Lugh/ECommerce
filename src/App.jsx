b1e950026c5f2741b5572fe44bc7c5770d9ac515
Jay-Lugh <jaylugh23@gmail.com>
2024-10-18 02:12:56 +0800

    Add UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Dashboard from './assets/Pages/Dashboard.jsx';
 import Profile from './assets/Pages/Profile.jsx';
 import ProductList from './assets/Pages/ProductList.jsx';
 import Navigation from './assets/Components/Navigation';
+import Edit from './assets/Pages/Edit.jsx';
+import Add from './assets/Pages/Add.jsx';
 
 
 function App(){
@@ -15,6 +17,36 @@ function App(){
   const toggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
   };
+  //EXAMPLE OUTPUT FOR DISPLAYING PLEASE ERASE WHEN INTEGRATING LARAVEL
+  const recentPurchases = [
+    {
+        image: 'https://via.placeholder.com/50', // Example image URL
+        name: 'Product 1',
+        category: 'Electronics',
+        description: 'Description for Product 1',
+        qty: 2,
+        price: 99.99,
+        barcode: '1234567890123',
+    },
+    {
+        image: 'https://via.placeholder.com/50',
+        name: 'Product 2',
+        category: 'Books',
+        description: 'Description for Product 2',
+        qty: 1,
+        price: 14.99,
+        barcode: '2345678901234',
+    },
+    {
+        image: 'https://via.placeholder.com/50',
+        name: 'Product 3',
+        category: 'Clothing',
+        description: 'Description for Product 3',
+        qty: 5,
+        price: 29.99,
+        barcode: '3456789012345',
+    },
+];
 
   return (
     <>
@@ -24,19 +56,22 @@ function App(){
 
       <div className="mainContent"
       style={{
-        marginLeft: isCollapsed ? '4%' : '10%',
+        marginLeft: isCollapsed ? '5%' : '10%',
         flexgrow: 1,
         transition: 'ease-in-out 0.5s'
       }} >
         <div className="scrollContainer">
         <Routes>
-        <Route path="/" element={<Dashboard/>} />
+        <Route path="/" element={<Dashboard items={recentPurchases}/>} />
         <Route path="/profile" element={<Profile/>} />
-        <Route path="/productlist" element={<ProductList/>} />
+        <Route path="/productlist" element={<ProductList items ={recentPurchases}/>} />
+        <Route path="/edit/:barcode" element={<Edit />} />
+        <Route path="/add" element={<Add />} />
       </Routes>
         </div>
       </div>
     </BrowserRouter>
+
     </>
       
       
