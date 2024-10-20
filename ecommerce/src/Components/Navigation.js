da9473b3a461011ebaf789698f528a836bc23078
Jay-Lugh <jaylugh23@gmail.com>
2024-10-20 15:05:45 +0800

    change in navigation icons

--- ecommerce/src/Components/Navigation.js
+++ ecommerce/src/Components/Navigation.js
@@ -6,10 +6,12 @@ import logo from "../profile.png";
 import './Components.css'; 
 import { FaBars } from 'react-icons/fa';
 import { IoMdLogOut } from "react-icons/io";
-
+import { CgProfile } from "react-icons/cg";
+import { MdDashboard } from "react-icons/md";
+import { IoBagHandleSharp } from "react-icons/io5";
 
 const Navigation = ({ isCollapsed, toggleSidebar }) => {
- 
+  const iconStyle = { fontSize: '30px' };
   return (
     <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
       <Button onClick={toggleSidebar} className="burgerButton">
@@ -17,9 +19,9 @@ const Navigation = ({ isCollapsed, toggleSidebar }) => {
       </Button>
       <Navbar bg="#343a40" variant='dark' className={`flex-column ${isCollapsed ? 'collapsed' : ''}`}>
         <Nav className="flex-column">
-         <Nav.Link as={Link} to="/profile"> <img src={logo} alt="Logo"/></Nav.Link>
-          <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-          <Nav.Link as={Link} to="/productlist">Product</Nav.Link>
+         <Nav.Link as={Link} to="/profile"> {isCollapsed ? <CgProfile  style={iconStyle} /> : <img src={logo} alt="Logo"/>} </Nav.Link>
+          <Nav.Link as={Link} to="/">{isCollapsed ? <MdDashboard  style={iconStyle}/>: "Dashboard"}</Nav.Link>
+          <Nav.Link as={Link} to="/productlist">{isCollapsed ? <IoBagHandleSharp  style={iconStyle}/> : "Product"}</Nav.Link>
           <Button onClick={toggleSidebar} className="logoutButton">
           <IoMdLogOut />
         </Button>
