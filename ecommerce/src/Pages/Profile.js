4d2d3aa71bcd2e982a71df827a892881b2555284
Jay-Lugh <jaylugh23@gmail.com>
2024-10-20 23:59:40 +0800

    profile

--- ecommerce/src/Pages/Profile.js
+++ ecommerce/src/Pages/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import profile from "../profile.png";
-
-
+import Form from 'react-bootstrap/Form';
 
 const Profile = () => {
   const employee = {
@@ -12,33 +11,50 @@ const Profile = () => {
     avatar: profile
   };
 
-
   return (
     <div className="profile">
 
       <div className="profile-header">
         <img src={employee.avatar} alt={`${employee.name}'s avatar`} className="profile-avatar" />
-        <h2>{employee.name}</h2>
-        <h4>{employee.position}</h4>
       </div>
 
-      <div className="profile-contact">
-        <p>Email: <a href={`mailto:${employee.email}`}>{employee.email}</a></p>
-        <p>Phone: <a href={`tel:${employee.phone}`}>{employee.phone}</a></p>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <p>This is the end.</p>
-      </div>
+      <Form className="profile">
+        <Form.Group controlId="formName">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control 
+            type="text" 
+            value={employee.name}
+            readOnly
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formPosition">
+          <Form.Label>Position:</Form.Label>
+          <Form.Control 
+            type="text" 
+            value={employee.position}
+            readOnly
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control 
+            type="email" 
+            value={employee.email}
+            readOnly
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formPhone">
+          <Form.Label>Phone:</Form.Label>
+          <Form.Control 
+            type="tel" 
+            value={employee.phone}
+            readOnly
+          />
+        </Form.Group>
+      </Form>
 
     </div>
   );
