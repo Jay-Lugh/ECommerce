8a822fd2188066c09de315efff07fd54229be2ec
Jay-Lugh <jaylugh23@gmail.com>
2024-10-21 21:51:51 +0800

    edit on navigation profile

--- ecommerce/src/Components/Navigation.js
+++ ecommerce/src/Components/Navigation.js
@@ -8,10 +8,16 @@ import { FaBars } from 'react-icons/fa';
 import { IoMdLogOut } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import { MdDashboard } from "react-icons/md";
-import { IoBagHandleSharp } from "react-icons/io5";
+import { IoBagHandleSharp } from "react-icons/io5";  
+import { useNavigate } from 'react-router-dom';
 
 const Navigation = ({ isCollapsed, toggleSidebar }) => {
   const iconStyle = { fontSize: '30px' };
+  const navigate = useNavigate(); 
+
+  const handleBack = () => {
+    navigate('/'); 
+};
   return (
     <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
       <Button onClick={toggleSidebar} className="burgerButton">
@@ -22,7 +28,7 @@ const Navigation = ({ isCollapsed, toggleSidebar }) => {
          <Nav.Link as={Link} to="/profile"> {isCollapsed ? <CgProfile  style={iconStyle} /> : <img src={logo} alt="Logo"/>} </Nav.Link>
           <Nav.Link as={Link} to="/dashboard">{isCollapsed ? <MdDashboard  style={iconStyle}/>: "Dashboard"}</Nav.Link>
           <Nav.Link as={Link} to="/productlist">{isCollapsed ? <IoBagHandleSharp  style={iconStyle}/> : "Product"}</Nav.Link>
-          <Button onClick={toggleSidebar} className="logoutButton">
+          <Button onClick={handleBack} className="logoutButton">
           <IoMdLogOut />
         </Button>
         </Nav>
